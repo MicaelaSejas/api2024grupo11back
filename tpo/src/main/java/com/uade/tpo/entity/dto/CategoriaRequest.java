600dd52eef51b2cdcacebea1646e0a9324b6242f
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2024-05-31 19:08:56 -0300

    merge

--- tpo/src/main/java/com/uade/tpo/entity/dto/CategoriaRequest.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.entity.dto;
-
-import lombok.Data;
-
-@Data
-public class CategoriaRequest {
-    private Long id;
-    private String descripcion;
-}
--- tpo/src/main/java/com/uade/tpo/entity/dto/DescuentoRequest.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpo.entity.dto;
-
-import lombok.Data;
-
-@Data
-public class DescuentoRequest {
-    private Long id;
-    private int descuento;
-}
