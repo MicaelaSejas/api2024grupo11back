93632c79bdc8ca1e02336ac84031bfdb8a52ee1d
BocanegraKleyver <kbocanegracisneros@uade.edu.ar>
2024-07-08 18:47:47 -0300

    Union ComprarScreen

--- tpo/src/main/java/com/uade/tpo/controller/config/SecurityConfig.java
+++ tpo/src/main/java/com/uade/tpo/controller/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.uade.tpo.controller.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
-import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,9 +30,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/error/**").permitAll()
-                        .requestMatchers("/api/v1/categoria").hasAnyAuthority("Vendedor")
-                        
-                        .requestMatchers("/api/v1/descuento").hasAnyAuthority("Vendedor")
+                        // .requestMatchers("/api/v1/categoria").hasAnyAuthority("Vendedor")
+                        .requestMatchers("/api/v1/categoria").permitAll()
+                        // .requestMatchers("/api/v1/descuento").hasAnyAuthority("Vendedor")
+                        .requestMatchers("/api/v1/descuento").permitAll()
                         // TODO: chequear que un comprador no pueda crear productos
                         .requestMatchers("/api/v1/producto").permitAll()
 //                        .requestMatchers("/carrito").authenticated()
